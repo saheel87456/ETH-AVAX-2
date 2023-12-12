d23768d9752661639f86ec982c13a0e32dc17da7
saheel87456 <151958451+saheel87456@users.noreply.github.com>
2023-12-13 00:14:23 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,41 +1,31 @@
-## saheel 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
 contract Assessment {
     address payable public owner;
     uint256 public balance;
-    uint256 public pin;
-    uint256 public loanAmount;
-    uint256 public loanTerm; // in months
-    uint256 public monthlyRepayment;
+    uint256 public goldLoanAmount;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
-    event PinChanged(uint256 newPin);
-    event LoanRequested(uint256 loanAmount, uint256 loanTerm, uint256 monthlyRepayment);
+    event GoldLoanRequested(uint256 amount);
+    event GoldLoanRepaid(uint256 amount);
+    event GoldLoanForgiven();
 
-    constructor(uint256 initBalance, uint256 _pin) payable {
+    constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
-        pin = _pin;
     }
 
-    function getBalance() public view returns (uint256) {
+    function getBalance() public view returns(uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
+        uint _previousBalance = balance;
         require(msg.sender == owner, "You are not the owner of this account");
-        uint256 _previousBalance = balance;
-
-        // perform transaction
         balance += _amount;
-
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
-
-        // emit the event
         emit Deposit(_amount);
     }
 
@@ -43,48 +33,45 @@ contract Assessment {
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint256 _previousBalance = balance;
+        uint _previousBalance = balance;
         if (balance < _withdrawAmount) {
-            revert
-                InsufficientBalance({
-                    balance: balance,
-                    withdrawAmount: _withdrawAmount
-                });
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
         }
-
-        // withdraw the given amount
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
         emit Withdraw(_withdrawAmount);
     }
 
-    function changePin(uint256 _newPin) public {
+    function requestGoldLoan(uint256 _loanAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        pin = _newPin;
-
-        // emit the event
-        emit PinChanged(_newPin);
+        require(goldLoanAmount == 0, "You already have an outstanding gold loan");
+        goldLoanAmount = _loanAmount;
+        emit GoldLoanRequested(_loanAmount);
     }
 
-    function getPin() public view returns (uint256) {
+    function payBackGoldLoan(uint256 _repaymentAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        return pin;
+        require(goldLoanAmount > 0, "No outstanding gold loan to repay");
+        goldLoanAmount -= _repaymentAmount;
+        emit GoldLoanRepaid(_repaymentAmount);
     }
 
-    function requestLoan(uint256 _requestedLoanAmount, uint256 _loanTermInMonths) public {
+    function getGoldLoanAmount() public view returns(uint256) {
+        return goldLoanAmount;
+    }
+
+    function forgiveGoldLoan() public {
         require(msg.sender == owner, "You are not the owner of this account");
-        require(_requestedLoanAmount > 0, "Loan amount must be greater than 0");
-        require(_loanTermInMonths > 0, "Loan term must be greater than 0");
+        goldLoanAmount = 0;
+        emit GoldLoanForgiven();
+    }
 
-        loanAmount = _requestedLoanAmount;
-        loanTerm = _loanTermInMonths;
-        monthlyRepayment = (_requestedLoanAmount / _loanTermInMonths);
-
-        // emit the event
-        emit LoanRequested(_requestedLoanAmount, _loanTermInMonths, monthlyRepayment);
+    function modifyGoldLoan(uint256 _newLoanAmount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        goldLoanAmount = _newLoanAmount;
+        emit GoldLoanRequested(_newLoanAmount);
     }
 }
